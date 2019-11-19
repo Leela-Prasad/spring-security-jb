d97a326a4186eaea50c50a1848971f4f072b67e0
Leela <leela.jagu@gmail.com>
2019-11-19 22:09:36 +0530

    Initial commit

---
+++ workspace/spring-security-examples/src/main/java/io/jb/spring/security/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.jb.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
